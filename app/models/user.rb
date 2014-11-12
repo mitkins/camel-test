cbdb7aeaa120e8e6ae656c97a6ccd0e109c3aff4
Hamish Murphy <hamishmurphy@gmail.com>
2014-11-12 23:19:09 +1100

    Updated frontend for the new authentication system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+    has_many :statuses
+
+    def full_name
+        result = ""
+        if ( first_name != nil )
+            result = first_name
+        end
+
+        if ( last_name != nil )
+            result = result + " " + last_name
+        end
+        
+        #first_name + " " + last_name
+
+        result
+    end
 end
