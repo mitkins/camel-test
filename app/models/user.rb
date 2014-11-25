00ffbb057ef6685d8987ba889dcd1d3cb9b3f859
Hamish Murphy <hamishmurphy@gmail.com>
2014-11-25 22:05:56 +1100

    Added gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,13 @@ class User < ActiveRecord::Base
 
         result
     end
+
+    def gravatar_url
+      stripped_email = email.strip
+      downcased_email = stripped_email.downcase
+
+      hash = Digest::MD5.hexdigest( downcased_email )
+
+      "http://gravatar.com/avatar/#{hash}"
+    end
 end
